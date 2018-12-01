364fa49ddd106221daceb03288b1dcb897ea843d
thinkpad <lihairuizone@sina.com>
2018-12-01 17:25:30 +0800

    first project

---
+++ helloGit/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author laoganbu
+ * @date: 2018/12/1 17:20
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+            System.out.println("Hello Git");
+    }
+
+}
